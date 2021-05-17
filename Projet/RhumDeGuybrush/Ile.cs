c7502469d93e3dcd66162ea3466221fef9c3cd98
Thomas-Gronnier <thomas.gronnier@etud.u-picardie.fr>
2021-05-17 10:24:12 +0200

    Creation class Ile

---
+++ Projet/RhumDeGuybrush/Ile.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RhumDeGuybrush
+{
+    class Ile
+    {
+        double[,] Carte =
+        {
+            {0,0},
+            {0,0}
+        };
+        string nomIle;
+
+        string Decodage(string a)
+        {
+            return a;
+        }
+
+        int affichageGlobal(int b)
+        {
+            return b;
+        }
+
+        int affichageListeParcelle(int c)
+        {
+            return c;
+        }
+
+        int affichageTailleParcelle(int d)
+        {
+            return d;
+        }
+
+        int affichagePlusGrandeParcelle(int e)
+        {
+            return e;
+        }
+
+        int affichageTailleMoyenne(int f)
+        {
+            return f;
+        }
+    }
+}
