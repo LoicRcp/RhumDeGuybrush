44f76ed0e8cae07083d8a945c5bdfcf1e84312b9
Pierre Gronnier <pierre.gronnier@etud.u-picardie.fr>
2021-05-17 10:31:55 +0200

    Création classe codage

---
+++ Projet/RhumDeGuybrush/Codage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RhumDeGuybrush
+{
+    class Codage
+    {
+        /// <summary>
+        /// On prend une carte en clair ( ile.afficheCarte() )et on suit les instruction du PDF pour le cryptage (pas compliqué du coup)
+        /// </summary>
+        /// <returns></returns>
+        int Codage()
+        {
+
+        }
+    }
+}
