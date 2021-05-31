4a51549e5d14bcd0b72cb495cbf4f570e1f7de3e
Loic Récoupé <loic.recoupe@gmail.com>
2021-05-31 15:18:32 +0200

    Fonction décodage fini + création classe Land + réorganisation légère autres classes/fonction due a l'implémentation de la classe Land

--- Projet/RhumDeGuybrush/Ile.cs
+++ Projet/RhumDeGuybrush/Ile.cs
@@ -13,10 +13,7 @@ namespace RhumDeGuybrush
         /// <summary>
         /// Talbeau 2D dans laquel est stocké la carte
         /// </summary>
-        char[,] carte = new char[3, 3]
-            { {'a','a','b' },
-              {'a','a','b' },
-              {'c','c','c' } };
+        Land[,] carte = new Land[10, 10];
 
         /// <summary>
         /// Nom de l'ile sur laquelle on travaille
@@ -35,11 +32,101 @@ namespace RhumDeGuybrush
         /// </summary>
         /// <param name="a"></param>
         /// <returns></returns>
-        string Decodage(string a)
+        void Decodage(string a)
         {
+            List<Land> parcelle = new List<Land>();
+            List<Land> checkNeighboor(List<Land> toCheck, Land[,] tabCode)
+            {
+                // List<Land> oldList = toCheck; // Problème de copie, il faut procéder autrement
+                // List<Land> newList = toCheck;
+
+                List<Land> oldList = new List<Land>();
+                List<Land> newList = new List<Land>();
+                /*
+                foreach (Land land in toCheck)
+                {
+                    parcelle.Add(land);
+                }
+                */
+
+                foreach (Land land in parcelle)
+                {
+                    if (land.done != true)
+                    {
+                        try { newList.Add(tabCode[land.y + 1, land.x]); } // Si y'as un élément en bas, l'ajoute dans la liste
+                        catch (Exception e) { }
+
+                        try { newList.Add(tabCode[land.y - 1, land.x]); } // Si y'as un élément en haut, l'ajoute dans la liste
+                        catch (Exception e) { }
+
+                        try { newList.Add(tabCode[land.y, land.x + 1]); } // Si y'as un élément a droite, l'ajoute dans la liste
+                        catch (Exception e) { }
+
+                        try { newList.Add(tabCode[land.y, land.x - 1]); } // Si y'as un élément a gauche, l'ajoute dans la liste
+                        catch (Exception e) { }
 
 
-            string[,] tabCode = new string[10, 10];
+
+
+
+
+                        if (tabCode[land.y, land.x].frontiereGauche || tabCode[land.y, land.x - 1].done)
+                        {
+                            try { newList.Remove(tabCode[land.y, land.x - 1]); }
+                            catch (Exception e) { }
+
+                        }
+                        if (tabCode[land.y, land.x].frontiereDroit || tabCode[land.y, land.x + 1].done)
+                        {
+                            try { newList.Remove(tabCode[land.y, land.x + 1]); }
+                            catch (Exception e) { }
+
+                        }
+                        if (tabCode[land.y, land.x].frontiereBas || tabCode[land.y + 1, land.x].done)
+                        {
+                            try { newList.Remove(tabCode[land.y + 1, land.x]); }
+                            catch (Exception e) { }
+
+                        }
+                        if (tabCode[land.y, land.x].frontiereHaut || tabCode[land.y - 1, land.x].done)
+                        {
+                            try { newList.Remove(tabCode[land.y - 1, land.x]); }
+                            catch (Exception e) { }
+
+                        }
+
+
+
+
+
+                        land.done = true;
+
+
+
+                    }
+
+                }
+
+                newList = newList.Distinct().ToList();
+                if (newList.Capacity > 0)
+                {
+
+
+                    foreach (Land toAdd in newList) { parcelle.Add(toAdd); };
+
+                    checkNeighboor(newList, tabCode);
+
+
+                }
+                return newList;
+            }
+
+
+
+
+
+
+            Land[,] tabCode = new Land[10, 10];
 
 
             string code = "67:69:69:69:69:69:69:69:69:73|74:3:9:7:5:13:3:1:9:74|74:2:8:7:5:13:6:4:12:74|74:6:12:7:9:7:13:3:9:74|74:3:9:11:6:13:7:4:8:74|74:6:12:6:13:11:3:13:14:74|74:7:13:7:13:10:10:3:9:74|74:3:1:9:7:12:14:2:8:74|74:6:4:4:5:5:13:6:12:74|70:69:69:69:69:69:69:69:69:76|";
@@ -53,11 +140,11 @@ namespace RhumDeGuybrush
             {
                 if (line != "")
                 {
-                    Console.WriteLine();
+                    //Console.WriteLine();
                     foreach (string row in line.Split(':'))
                     {
-                        tabCode[i, j] = row;
-                        Console.Write(tabCode[i, j] + ":");
+                        tabCode[i, j] = new Land(row, i, j);
+                        //Console.Write(tabCode[i, j].Nb + ":");
                         j++;
                     }
                     j = 0;
@@ -65,8 +152,106 @@ namespace RhumDeGuybrush
                 }
             }
 
+            Land[,] decodedTab = new Land[10, 10];
+            int charCounter = 96;
 
 
+            System.ConsoleColor[] colors = new System.ConsoleColor[5] { ConsoleColor.Red, ConsoleColor.Yellow, ConsoleColor.DarkGray, ConsoleColor.Cyan, ConsoleColor.Magenta };
+            int colorCounter = 0;
+            System.ConsoleColor RandomColor = colors[colorCounter];
+
+            for (i = 0; i < 10; i++)
+            {
+                for (j = 0; j < 10; j++)
+                {
+                    List<Land> toCheck = new List<Land>();
+                    toCheck.Add(tabCode[i, j]);
+
+                    parcelle = new List<Land>();
+                    parcelle.Add(tabCode[i, j]);
+
+                    checkNeighboor(parcelle, tabCode);
+
+
+
+
+
+
+
+                    int count = 0;
+
+
+
+                    foreach (Land land in parcelle)
+                    {
+
+
+
+                        count++;
+                    }
+
+
+
+
+                    if (count > 1) // sinon ça affiche pour chaque itération, le bout de terre sur lequel on est. ça règle pas le problème, ça le contourne, mais j'ai bien la flemme la
+                    {
+                        if (colorCounter >= 4)
+                        {
+                            colorCounter = 0;
+                        }
+                        else
+                        {
+                            colorCounter++;
+                        }
+
+                        RandomColor = colors[new Random().Next(5)];
+                        if (!parcelle[0].foret && !parcelle[0].mer)
+                        {
+                            charCounter++;
+                        }
+
+
+                        foreach (Land land in parcelle)
+                        {
+                            land.color = RandomColor;
+
+                            if (land.mer)
+                            {
+                                land.lettre = 'M';
+                                land.color = ConsoleColor.Blue;
+                            }
+                            else if (land.foret)
+                            {
+                                land.lettre = 'F';
+                                land.color = ConsoleColor.Green;
+                            }
+                            else
+                            {
+                                land.lettre = (char)charCounter;
+                            }
+
+
+                            decodedTab[land.y, land.x] = land;
+
+                        }
+                    }
+
+                }
+            }
+
+
+            for (i = 0; i < 10; i++)
+            {
+                Console.WriteLine();
+
+                for (j = 0; j < 10; j++)
+                {
+                    Console.ForegroundColor = decodedTab[i, j].color;
+                    Console.Write(decodedTab[i, j].lettre);
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Write(":");
+                }
+            }
             // convertion string --> tableau 2D encodé - Fait
             // A faire: Fonction récursive, je parcours le tableau, je check pour les frontières, etc...
 
@@ -74,8 +259,9 @@ namespace RhumDeGuybrush
 
 
 
+            carte = decodedTab;
+            
 
-            return a;
         }
 
         /// <summary>
---
+++ Projet/RhumDeGuybrush/Land.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RhumDeGuybrush
+{
+     class Land
+    {
+        public string nb; // Le nombre si land = encodé
+        public char lettre; // la lettre si land = décodé
+
+        public System.ConsoleColor color;
+
+        public int x; // Coordonnées x (dans le tableau) de l'élément
+        public int y; // ........... y ..............................
+
+        public Boolean frontiereHaut = false;
+        public Boolean frontiereBas = false;
+        public Boolean frontiereGauche = false;
+        public Boolean frontiereDroit = false;
+        public Boolean mer = false;
+        public Boolean foret = false; // les paramètres
+
+        public Boolean done = false; // pour savoir si on va faire un check avec la fonctio un récursive dessus - C'est de l'optimisation en gros
+
+        public Land(string _nb, int _y, int _x) // Constructeur pour un bout de terre encodé (on donne que le nombre)
+        {
+
+            x = _x;
+            y = _y;
+            nb = _nb;
+            int intNb = Convert.ToInt32(_nb);
+
+            if (intNb - 64 >= 0)
+            {
+                mer = true;
+                intNb -= 64;
+            }
+            else if (intNb - 32 >= 0)
+            {
+                foret = true;
+                intNb -= 32;
+            }
+
+            if (intNb - 8 >= 0)
+            {
+                frontiereDroit = true;
+                intNb -= 8;
+            }
+            if (intNb - 4 >= 0)
+            {
+                frontiereBas = true;
+                intNb -= 4;
+            }
+            if (intNb - 2 >= 0)
+            {
+                frontiereGauche = true;
+                intNb -= 2;
+            }
+            if (intNb - 1 >= 0)
+            {
+                frontiereHaut = true;
+                intNb -= 1;
+            }
+
+
+
+            // Console.WriteLine("haut: {0}\nbas: {1}\ngauche: {2}\ndroite: {3}\nmer: {4}\nforet:{5}\n", frontiereHaut, frontiereBas, frontiereGauche, frontiereDroit, mer, foret);
+
+        }
+
+
+    
+
+        Land(char lettre, Boolean frontHaut, Boolean frontBas, Boolean frontGauche, Boolean frontDroite, Boolean mer, Boolean foret) // Constructeur pour un bout de terre décodé, on donne son nom de parcelle et ses propriétés
+        {
+
+
+
+        }
+
+
+
+        public string Nb
+        {
+            get { return nb; }
+        }
+    }
+}
