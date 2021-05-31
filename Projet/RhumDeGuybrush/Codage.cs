cabaf248dcd69aac866369bac21d10033577dd01
Thomas-Gronnier <thomas.gronnier@etud.u-picardie.fr>
2021-05-31 12:02:36 +0200

    Documentation des fonctions

--- Projet/RhumDeGuybrush/Codage.cs
+++ Projet/RhumDeGuybrush/Codage.cs
@@ -4,94 +4,127 @@ using System.Text;
 
 namespace RhumDeGuybrush
 {
+
+  
     static class Codage
     {
         /// <summary>
-        /// On prend une carte en clair ( ile.afficheCarte() )et on suit les instruction du PDF pour le cryptage (pas compliqué du coup)
+        /// Fonction FrontiereGauche regarde le chiffre à gauche et retourne faux si il est égal. Si non égal ou tentative d'accès à case d'accès du tableau inexistante retourne vrai  
+        /// <returns>Booleen vrai si frontière gauche</returns>
         /// </summary>
-        /// <returns></returns>
+        /// <param name="carte">Tableau qui représente la carte></param>
+        /// <param name="i">Curseur lignes></param>
+        ///  <param name="j">Curseur colonnes></param>
+        public static Boolean FrontiereGauche(char[,] carte, int i, int j)
+        {
+            try
+            {
+                if (carte[i, j - 1] == carte[i, j])
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+
+            catch (Exception e)
+            {
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// Fonction FrontiereDroite regarde le chiffre à droite et retourne faux si il est égal. Si non égal ou tentative d'accès à case d'accès du tableau inexistante retourne vrai  
+        /// <returns>Booleen vrai si frontière droite</returns>
+        /// </summary>
+        /// <param name="carte">Tableau qui représente la carte></param>
+        /// <param name="i">Curseur lignes></param>
+        ///  <param name="j">Curseur colonnes></param>
+        public static Boolean FrontiereDroite(char[,] carte, int i, int j)
+        {
+            try
+            {
+                if (carte[i, j + 1] == carte[i, j])
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+
+            catch (Exception e)
+            {
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// Fonction FrontiereHaut regarde le chiffre à droite et retourne faux si il est égal. Si non égal ou tentative d'accès à case d'accès du tableau inexistante retourne vrai  
+        /// <returns>Booleen vrai si frontière en haut</returns>
+        /// </summary>
+        /// <param name="carte">Tableau qui représente la carte></param>
+        /// <param name="i">Curseur lignes></param>
+        ///  <param name="j">Curseur colonnes></param>
+        public static Boolean FrontiereHaut(char[,] carte, int i, int j)
+        {
+            try
+            {
+                if (carte[i - 1, j] == carte[i, j])
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+
+            catch (Exception e)
+            {
+                return true;
+            }
+        }
+
+
+        /// <summary>
+        /// Fonction FrontiereBas regarde le chiffre à droite et retourne faux si il est égal. Si non égal ou tentative d'accès à case d'accès du tableau inexistante retourne vrai  
+        /// <returns>Booleen vrai si frontière en bas</returns>
+        /// </summary>
+        /// <param name="carte">Tableau qui représente la carte></param>
+        /// <param name="i">Curseur lignes></param>
+        ///  <param name="j">Curseur colonnes></param>
+
+        public static Boolean FrontiereBas(char[,] carte, int i, int j)
+        {
+            try
+            {
+                if (carte[i + 1, j] == carte[i, j])
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+
+            catch (Exception e)
+            {
+                return true;
+            }
+        }
+        /// <summary>
+        /// Fonction Codage regarde les frontières et ajoute le numéro adapté en conséquences ainsi que le type de terre (Mer/Foret) pour chaque élément et construit a
+        /// </summary>
+       
         static void codage()
         {
             string encode = "";
-            Boolean FrontiereGauche(char[,] carte, int i, int j)
-            {
-                try
-                {
-                    if (carte[i, j - 1] == carte[i, j])
-                    {
-                        return false;
-                    }
-                    else
-                    {
-                        return true;
-                    }
-                }
-
-                catch (Exception e)
-                {
-                    return true;
-                }
-            }
-
-            Boolean FrontiereDroite(char[,] carte, int i, int j)
-            {
-                try
-                {
-                    if (carte[i, j + 1] == carte[i, j])
-                    {
-                        return false;
-                    }
-                    else
-                    {
-                        return true;
-                    }
-                }
-
-                catch (Exception e)
-                {
-                    return true;
-                }
-            }
-
-            Boolean FrontiereHaut(char[,] carte, int i, int j)
-            {
-                try
-                {
-                    if (carte[i - 1, j] == carte[i, j])
-                    {
-                        return false;
-                    }
-                    else
-                    {
-                        return true;
-                    }
-                }
-
-                catch (Exception e)
-                {
-                    return true;
-                }
-            }
-
-            Boolean FrontiereBas(char[,] carte, int i, int j)
-            {
-                try
-                {
-                    if (carte[i + 1, j] == carte[i, j])
-                    {
-                        return false;
-                    }
-                    else
-                    {
-                        return true;
-                    }
-                }
-
-                catch (Exception e)
-                {
-                    return true;
-                }
-            }
+           
 
 
             char[,] carte = new char[3, 3]
